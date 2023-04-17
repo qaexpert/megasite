3f083843fabec6e29a7679f88d678c4b440d07c2
Dmitrii Zhabin <d_zhabin@mail.ru>
2023-04-18 02:06:54 +0300

    version 1.4.1-alpha.09

--- itfinder/settings.py
+++ itfinder/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path( __file__ ).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xi@$j8n80e0!_!ejrvsm7givxg_i8ingh*bqzn92@l^le-$)ss'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- itfinder/urls.py
+++ itfinder/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path('set-language/', set_language, name='set_language'),
 ]
 
-urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+if settings.DEBUG:
+    urlpatterns += static( settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
 urlpatterns += static( settings.STATIC_URL, document_root=settings.STATIC_ROOT )
 urlpatterns += i18n_patterns(
     path('', include('users.urls')), prefix_default_language=True
